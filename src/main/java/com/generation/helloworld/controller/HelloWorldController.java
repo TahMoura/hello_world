e8432346230b2c0ef3331c8d9fed0b09d45175a8
TaliaMoura <taliamouragit@outlook.com>
2023-12-10 19:28:08 -0300

    Implentação dos métodos que retornam as BSM's da Generation e meus objetivos de aprendizagem para essa semana.

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -8,9 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 	
-	@GetMapping
+	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World!!!";
 	}
-
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "BSM's da Generation Brasil:\n"
+				+"- Persistência\n"
+				+"- Mentalidade de Crescimento\n"
+				+"- Responsabilidade Pessoal\n"
+				+"- Orientação ao Futuro\n"
+				+"- Proatividade\n"
+				+"- Comunicação\n"
+				+"- Trabalho em Equipe\n"
+				+"- Orientação ao Detalhe";
+	}
+	
+	@GetMapping("/lista-objetivos")
+	public String listaObjetivos() {
+		return "Lista dos meus Objetivos de Aprendizagem para essa Semana:\n"
+				+"- Praticar e ver mais sobre Spring Framework\n"
+				+"- Praticar banco de Dados\n"
+				+"- Rever os vídeos sobre banco de dados e spring e reler os cookbooks para melhor fixação\n"
+				+"- Aprender mais sobre APIs\n"
+				+"- Aplicar melhor as boas práticas nos meus Projetos";
+	}
 }
